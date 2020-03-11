41d409dae064d3d943da77256ba9f890bc43ef27
alexanderskiba <58416654+alexanderskiba@users.noreply.github.com>
2020-03-11 16:37:24 +0300

    Add files via upload

---
+++ strtask.py
@@ -0,0 +1,23 @@
+
+# task = 'md'
+# lols = 'mdma mdlergv lkrlmd podfdgf'
+# newl = lols.split()
+# print('Исходная строка')
+# print(lols)
+# print('Слова с найденной подстрокой:')
+# for k in newl:
+#     if task in k:
+#         print (k, end=" ")
+
+keks = ' kek   shpek   cheburek '
+
+
+# a = '*'.join(keks.split())
+# print(a)
+# a=0
+# for x in keks:
+#     a+=1
+#     print(x,end="*" if a<len(keks) else "")
+lst = ['1', '2', '3']
+
+print(', '.join(lst))
