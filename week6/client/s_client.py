67368ed0dba166d951a1ff5c8bc0742838b5bb2a
Alexander Skiba <vetrolet3000@gmail.com>
2020-05-18 13:22:30 +0300

    add simple_client

---
+++ week6/client/s_client.py
@@ -0,0 +1,14 @@
+import socket
+import time
+
+with socket.create_connection(("127.0.0.1", 8888)) as sock:
+    key = 'palm.cpu'
+    value = 23.7
+    timestamp = 1150864248
+    key1 = 'palm.cpu'
+    value1 = 25
+    timestamp1 = 1000064248
+    sock.sendall(f"put {key} {value} {timestamp}\n".encode())
+    time.sleep(2)
+    sock.sendall(f"put {key1} {value1} {timestamp1}\n".encode())
+#    sock.sendall("Hello".encode("utf8"))
